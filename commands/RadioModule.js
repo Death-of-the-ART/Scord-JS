09ef8aadc6f9afc9ebe4416f08f1f684c275531b
Marcin <thegsgstudio@protonmail.com>
2021-06-16 15:45:51 +0200

    Update 3.2.0b4

--- commands/RadioModule.js
+++ commands/RadioModule.js
@@ -7,10 +7,10 @@ module.exports = {
         
         if (!channel) return console.error("|6|REQUEST_FAILED");
         channel.join().then(connection => {
-            console.log(`|3|[0]REQUEST_CONNECT - ${message.author.tag}`);
+            console.log(`|3|REQUEST_CONNECT[0] - ${message.author.tag}`);
             connection.play(process.env.STREAM, { volume: false }, { bitrate: 44100 }, { highWaterMark: 100 });
             message.client.user.setActivity(process.env.STATUS, { type: "LISTENING" })
-                .then(console.log(`|4|[0]REQUEST_SUCCESSFUL`), message.delete());
+                .then(console.log(`|4|REQUEST_SUCCESSFUL[0]`), message.delete());
         }).catch(e => {
             console.error(e);
         });
--- commands/RadioModule_2.js
+++ commands/RadioModule_2.js
@@ -7,10 +7,10 @@ module.exports = {
         
         if (!channel) return console.error("|6|REQUEST_FAILED");
         channel.join().then(connection => {
-            console.log(`|3|[2]REQUEST_CONNECT - ${message.author.tag}`);
+            console.log(`|3|REQUEST_CONNECT[2] - ${message.author.tag}`);
             connection.play(process.env.STREAM2, { volume: false }, { bitrate: 44100 }, { highWaterMark: 100 });
             message.client.user.setActivity(process.env.STATUS2, { type: "LISTENING" })
-                .then(console.log(`|4|[2]REQUEST_SUCCESSFUL`), message.delete());
+                .then(console.log(`|4|REQUEST_SUCCESSFUL[2]`), message.delete());
         }).catch(e => {
             console.error(e);
         });
